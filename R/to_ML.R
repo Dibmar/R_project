658077ec75988c42c0dc3fae2a077d174d48c465
Dibmar <diomedes.barbero@gmail.com>
2020-07-09 16:26:50 +0200

    09/07/2020 Dibmar. to_ML funcional. Coge la 5ª columna del df para hacer el entrenamiento

--- R/to_ML.R
+++ R/to_ML.R
@@ -5,22 +5,35 @@
 to_ML <- function(df_1){
   library(caret)
   library(logging)
+  library(dplyr)
   
-  # 1a columna seleccionada como target
+
+  # Split between TRAIN (validation_index, 80% of data) and TEST (validation, 20% of data) samples
   validation_index <- createDataPartition(df_1[, 1], p=0.80, list=FALSE)
   validation <- df_1[-validation_index,]
   
   dataset <- dataset[validation_index,]
   
+  
+  # For the training
+  x_train <- dataset[,-5]
+  y_train <- dataset[,5]
+  
+  
+  # Logging
   loginfo("Resumen estadístico", logger = 'log')
+
   dataset_summary <- summary(dataset)
+  
   loginfo(dataset_summary, logger = 'log')
   loginfo("Resumen terminado", logger = 'log')
   
+  
   # Simple linear regression model (lm means linear model)
-  model_1 <- train(mpg ~ wt,data = dataset, method = "lm")
+  model_1 <- train(x = x_train, y = as.numeric(y_train), method = "lm")
   
-  
+
+  # Logging results
   loginfo("Resumen de modelo de ML", logger = 'log')
   
   results <- resamples(model_1)
@@ -28,7 +41,11 @@ to_ML <- function(df_1){
   
   loginfo(results, logger = 'log')
   loginfo("Resumen de modelo de ML terminado", logger = 'log')
+
+
+  return(model_1, validation)
+
+ 
   
-  
-  return(model_1)
-}
+ }
+
